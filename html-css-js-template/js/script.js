1c9c6816b2042ea7654d32099e8f17459d591f5c
Iker Lobo Perez <ikerlobop@gmail.com>
2024-06-03 12:33:26 +0200

    nueva plantilla

---
+++ html-css-js-template/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const boton = document.getElementById('miBoton');
+    boton.addEventListener('click', () => {
+        alert('¡Botón clicado!');
+    });
+});
